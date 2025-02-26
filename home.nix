eb73a394b127c9191ae19b8b05221c53985be735
KingMalower <maciejzych2003@gmail.com>
2025-02-26 21:23:55 +0100

    First Commit

---
+++ home.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+
+  imports = [
+   ./modules/git.nix 
+  ];
+
+  home = {
+  username = "malower";
+  homeDirectory = "/home/malower";
+  stateVersion = "24.05"; # Please read the comment before changing.
+  packages = with pkgs; [
+    fastfetch
+    btop
+    gh
+  ];
+  };
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
